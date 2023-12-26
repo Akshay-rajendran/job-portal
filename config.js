a6ab0fd81981c43ed33486e37701c2eea49e03d1
akshayapv98 <akshayapv15@gmail.com>
2023-12-26 22:33:19 +0530

    console

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ async function connectDb(){
   try {
     mongoose.set('strictQuery', false);
     mongoose.connect("mongodb+srv://database:123@cluster0.n3lgztl.mongodb.net/jobportaldb?retryWrites=true&w=majority")
-    console.log("Db connected");
+    console.log("port 3000 running");
+    // console.log("Db connected");
+
   } catch (error) {
    console.log(error,"Db error");
   }
